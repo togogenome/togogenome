023ced7cbc69314fa2fa7f02e0e5cd3a343d4d7e
Naoki Nishiguchi <nnishiguchister@gmail.com>
2014-07-16 18:12:15 +0900

    ID converter のUI/デザイン変更

--- app/controllers/converter_controller.rb
+++ app/controllers/converter_controller.rb
@@ -6,8 +6,12 @@ class ConverterController < ApplicationController
     #identifiers ['P16033', 'G0JEW3']
     #databases ['uniprot', 'pfam', 'refseq', 'ec-code']
 
-    if identifiers.empty? || databases.nil?
+    if databases.nil? and identifiers.empty?
       @message = 'Input identifiers to be converted and select target and intermediate databases.'
+    elsif databases.nil?
+      @message = 'Select target and intermediate databases.'
+    elsif identifiers.empty?
+      @message = 'Input identifiers to be converted.'
     else
       @db_links = Identifier.convert(identifiers, databases)
       @message = 'Identifiers not found.' if @db_links.empty?
