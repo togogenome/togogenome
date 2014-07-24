de2b7223709198e9cbe79f526163f049d72dbd97
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2014-07-24 14:56:32 +0900

    Stanza のテキスト検索 API 呼出しをワンパス通した

--- app/models/text_search.rb
+++ app/models/text_search.rb
@@ -1,19 +1,39 @@
+require 'open-uri'
+
 class TextSearch
   class << self
     def search(q)
-      {
-        stanzas: [
-          {
-            name: 'stanza name',
-            count: 3,
-            urls: [
-              'http://example.com/1',
-              'http://example.com/2',
-              'http://example.com/3'
-            ]
-          }
-        ]
+      # [togostanza] の [organisms] のスタンザを検索
+      stanza_root = Stanza.providers.togostanza.url
+      name_and_urls = Stanza.providers.togostanza.organisms.map {|stanza|
+        {name: stanza.name, url: "#{stanza_root}/#{stanza.id}/text_search?q=#{q}"}
       }
+      name_and_urls.map {|e|
+        name = e[:name]
+        url = e[:url]
+        res = JSON.parse(open(url).read).with_indifferent_access
+        {
+          name: "#{name} (#{res[:count]})",
+          count: res[:count],
+          urls: res[:urls]
+        }
+      }
+      # [
+      #   {
+      #     name: 'stanza name',
+      #     count: 3,
+      #     urls: [
+      #       'http://example.com/1',
+      #       'http://example.com/2',
+      #       'http://example.com/3'
+      #     ]
+      #   },
+      #   {
+      #     name: 'stanza name again',
+      #     count: 0,
+      #     urls: []
+      #   }
+      # ]
     end
   end
 end
