39f7b401cf2f5434e31ee28be7eb6d7ad85f8e57
Naoki Nishiguchi <nnishiguchister@gmail.com>
2014-09-04 17:39:43 +0900

    Fix spec

--- spec/models/text_search_spec.rb
+++ spec/models/text_search_spec.rb
@@ -1,30 +1,28 @@
 require 'spec_helper'
 
 describe TextSearch do
-  describe 'self.search_stanza' do
+  describe 'self.search_by_stanza_id' do
     context "'Organism name' スタンザに対して 'PCC71' でテキスト検索する" do
       subject {
-        TextSearch.search_stanza('organism_names', 'PCC71')
+        TextSearch.search_by_stanza_id('PCC71', 'organism_names')
       }
 
-      it { should include(id: 'organism_names') }
+      it { should include(stanza_id: 'organism_names') }
       it { should include(count: 10) }
       it { should include(report_type: 'organisms') }
-      it { subject[:url].should match /\/stanza\/organism_names/ }
+      it { subject[:stanza_url].should match /\/stanza\/organism_names/ }
       it {
-        should include(entry_ids:
-          [
-            {'tax_id' => '163908'},
-            {'tax_id' => '1168'},
-            {'tax_id' => '32057'},
-            {'tax_id' => '103690'},
-            {'tax_id' => '102127'},
-            {'tax_id' => '118166'},
-            {'tax_id' => '128403'},
-            {'tax_id' => '179408'},
-            {'tax_id' => '32055'},
-            {'tax_id' => '93135'}
-          ]
+        should include(urls:
+          ['http://localhost:9292/stanza/organism_names?tax_id=163908',
+           'http://localhost:9292/stanza/organism_names?tax_id=1168',
+           'http://localhost:9292/stanza/organism_names?tax_id=32057',
+           'http://localhost:9292/stanza/organism_names?tax_id=103690',
+           'http://localhost:9292/stanza/organism_names?tax_id=102127',
+           'http://localhost:9292/stanza/organism_names?tax_id=118166',
+           'http://localhost:9292/stanza/organism_names?tax_id=128403',
+           'http://localhost:9292/stanza/organism_names?tax_id=179408',
+           'http://localhost:9292/stanza/organism_names?tax_id=32055',
+           'http://localhost:9292/stanza/organism_names?tax_id=93135']
         )
       }
     end
