5ca733fff549a6663e23bbb5991eabf8153e36d1
Naoki Nishiguchi <nnishiguchister@gmail.com>
2014-09-05 16:55:35 +0900

    ページネーションのページリンク数を5個固定で出すようにした

--- app/helpers/text_helper.rb
+++ app/helpers/text_helper.rb
@@ -24,4 +24,20 @@ module TextHelper
     search_target = [['All', 'all'], ['Genes', 'gene_reports'], ['Organisms', 'organism_reports'], ['Environments', 'environment_reports']]
     search_target + Stanza.all.map{|s| [s['name'], s['id']] }
   end
+
+  def fixed_link_count_paginate(stanzas)
+    first_page = 1
+    last_page = stanzas.total_pages
+
+    window = case stanzas.current_page
+             when first_page, last_page
+               4
+             when (first_page + 1), (last_page - 1)
+               3
+             else
+               2
+             end
+
+    paginate @stanzas, window: window
+  end
 end
