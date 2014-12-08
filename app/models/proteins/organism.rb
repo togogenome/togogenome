f0639293f4bf6df8ac3bcbd97f3f2d6479c8d0aa
Naoki Nishiguchi <nnishiguchister@gmail.com>
2014-12-08 17:08:22 +0900

    Organism の表に Environment と Phenotype も出すようにした

--- app/models/proteins/organism.rb
+++ app/models/proteins/organism.rb
@@ -16,16 +16,37 @@ module Proteins
 
         return [] if results.empty?
 
+        targets = [
+          { name: 'envs',       sparql: find_environments_sparql( results.map {|b| "<#{b[:taxonomy_id]}>" }.uniq.join(' ') ) },
+          { name: 'phenotypes', sparql: find_phenotypes_sparql( results.map {|b| "<#{b[:taxonomy_id]}>" }.uniq.join(' ') ) }
+        ]
+
+
+        envs, phenotypes = nil, nil
+
+        Parallel.map(targets, in_threads: 4) {|target|
+          res = query(target[:sparql])
+          case target[:name]
+          when 'envs'       then envs = res
+          when 'phenotypes' then phenotypes = res
+          end
+        }
+
         results.map do |result|
-          new(result)
+          select_envs       = envs.select {|e| e[:taxonomy_id] == result[:taxonomy_id] }
+          select_phenotypes = phenotypes.select {|p| p[:taxonomy_id] == result[:taxonomy_id] }
+
+          new(result, select_envs, select_phenotypes)
         end
       end
     end
 
-    def initialize(up_tax)
+    def initialize(up_tax, envs, phenotypes)
       @tax = Taxonomy.new(up_tax[:taxonomy_id], up_tax[:taxonomy_name])
+      @envs       = envs.map {|env| Environment.new(env[:meo_id], env[:meo_name]) }
+      @phenotypes = phenotypes.map {|phenotype| Phenotype.new(phenotype[:mpo_id], phenotype[:mpo_name]) }
     end
 
-    attr_reader :tax
+    attr_reader :tax, :envs, :phenotypes
   end
 end
