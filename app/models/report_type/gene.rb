f5428d5aa33e53dd29d1e0d358f787526642b92f
Naoki Nishiguchi <nnishiguchister@gmail.com>
2015-01-06 16:52:23 +0900

    addition_information の実装はサブクラス側で定義させるようにした

--- app/models/report_type/gene.rb
+++ app/models/report_type/gene.rb
@@ -1,4 +1,31 @@
 module ReportType
   class Gene < Base
+    class << self
+      def addition_information(results)
+        upids  = results.map {|b| "<#{b[:uniprot_id]}>" }.uniq.join(' ')
+        uniport_ups = results.map {|b| "<#{b[:uniprot_up]}>" }.uniq.join(' ')
+        taxids = results.map {|b| "<#{b[:taxonomy_id]}>" }.uniq.join(' ')
+
+        sparqls = [
+          find_genes_sparql(upids),
+          find_gene_ontologies_sparql(uniport_ups)
+        ]
+
+        genes, gos = Parallel.map(sparqls, in_threads: 2) {|sparql|
+          query(sparql)
+        }
+
+        results.map do |result|
+          select_genes      = genes.select {|g| g[:uniprot_id] == result[:uniprot_id] }
+          select_gos        = gos.select {|g| g[:uniprot_up] == result[:uniprot_up] }
+
+          new(result, select_genes, select_gos)
+        end
+      end
+    end
+
+    def initialize(up_tax, genes, gos)
+      @uniprot_taxonomy, @genes, @gos = up_tax, genes, gos
+    end
   end
 end
