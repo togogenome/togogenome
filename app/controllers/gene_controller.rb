b9e4a6555d44c011dbfcad91d3466b95139a2b84
Naoki Nishiguchi <nnishiguchister@gmail.com>
2015-01-19 11:30:30 +0900

    gene uri 変更に伴う report page 側のスタンザ呼び出しを変更した

--- app/controllers/gene_controller.rb
+++ app/controllers/gene_controller.rb
@@ -2,6 +2,6 @@ class GeneController < ApplicationController
   def show(id)
     # "150340:NC_013456.1#feature:718674-719099:1:gene.715"
     #  => "150340", "NC_013456.1#feature:718674-719099:1:gene.715"
-    @taxonomic_id, @locus_tag = id.split(':', 2)
+    @refseq_id, @gene_id = id.split(':', 2)
   end
 end
