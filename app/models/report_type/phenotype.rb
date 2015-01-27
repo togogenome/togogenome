16841fa55dbcc6f9a135bb8bf2c8b7d76c75acef
Naoki Nishiguchi <nnishiguchister@gmail.com>
2015-01-27 14:57:44 +0900

    Facet検索で Phenotype と 該当Phenotypeの ルートノード を取得してみた

---
+++ app/models/report_type/phenotype.rb
@@ -0,0 +1,56 @@
+module ReportType
+  class Phenotype < Base
+    class << self
+      def count(meo_id: '', tax_id: '', bp_id: '', mf_id: '', cc_id: '', mpo_id: '')
+        select_clause =  "SELECT COUNT(DISTINCT ?mpo_id) AS ?hits_count"
+        sparql = build_phenotype_sparql(meo_id, tax_id, bp_id, mf_id, cc_id, mpo_id, select_clause)
+
+        results = query(sparql)
+
+        results.first[:hits_count]
+      end
+
+      def search(meo_id: '', tax_id: '', bp_id: '', mf_id: '', cc_id: '', mpo_id: '', limit: 25, offset: 0)
+        select_clause = "SELECT DISTINCT ?mpo_id ?mpo_name"
+        sparql = build_phenotype_sparql(meo_id, tax_id, bp_id, mf_id, cc_id, mpo_id, select_clause, limit, offset)
+
+        results = query(sparql)
+
+        return [] if results.empty?
+
+        mpos = results.map {|r| "<#{r[:mpo_id]}>" }.uniq.join(' ')
+
+        sparqls = [
+          find_phenotype_root_sparql(PREFIX, ONTOLOGY, mpos),
+          #find_phenotype_inhabitants_stats_sparql(PREFIX, ONTOLOGY, mpos)
+        ]
+
+        #mpo_roots, mpo_inhabitants_stats = Parallel.map(sparqls, in_threads: 4) {|sparql|
+        #  query(sparql)
+        #}
+        mpo_roots = query(sparqls.first)
+
+        results.map do |result|
+          select_mpo_roots = mpo_roots.select {|r| r[:mpo_id] == result[:mpo_id] }
+          #select_mpo_inhabitants_stats = mpo_inhabitants_stats.select {|r| r[:mpo_id] == result[:mpo_id] }
+          #new(result, select_mpo_roots, select_mpo_inhabitants_stats)
+          new(result, select_mpo_roots)
+        end
+      end
+    end
+
+    def initialize(mpo, mpo_roots)
+      @phenotype, @phenotype_roots = mpo, mpo_roots
+    end
+
+    def phenotype
+      root_name = @phenotype_roots.empty? ? nil : @phenotype_roots.first[:name]
+
+      Struct.new(:uri, :name, :root){
+        def id
+          uri.split('/').last
+        end
+      }.new(@phenotype[:mpo_id], @phenotype[:mpo_name], root_name)
+    end
+  end
+end
--- app/models/report_type/sparql_builder.rb
+++ app/models/report_type/sparql_builder.rb
@@ -50,6 +50,20 @@ module ReportType
         end
       end
 
+      def build_phenotype_sparql(meo_id, tax_id, bp_id, mf_id, cc_id, mpo_id, select_clause, limit = 1, offset = 0)
+        if [bp_id, mf_id, cc_id].any?(&:present?)
+          phenotypes_has_go_condition(PREFIX, ONTOLOGY, mpo_id, meo_id, tax_id, bp_id, mf_id, cc_id, select_clause, limit, offset)
+        elsif tax_id.present?
+          phenotypes_has_tax_condition(PREFIX, ONTOLOGY, mpo_id, meo_id, tax_id, select_clause, limit, offset)
+        elsif meo_id.present?
+          phenotypes_has_meo_condition(PREFIX, ONTOLOGY, mpo_id, meo_id, select_clause, limit, offset)
+        elsif mpo_id.present?
+          phenotypes_has_mpo_condition(PREFIX, ONTOLOGY, mpo_id, select_clause, limit, offset)
+        else
+          phenotypes_init_condition(PREFIX, ONTOLOGY, select_clause, limit, offset)
+        end
+      end
+
       extend ERB::DefMethod
       def_erb_method("genes_init_condition(prefix, ontology, select_clause, limit, offset)", 'app/views/sparql_templates/genes/init_condition.rq.erb')
       def_erb_method("genes_has_mpo_condition(prefix, ontology, mpo_id, select_clause, limit, offset)", 'app/views/sparql_templates/genes/has_mpo_condition.rq.erb')
@@ -69,6 +83,12 @@ module ReportType
       def_erb_method("environments_has_tax_condition(prefix, ontology, mpo_id, meo_id, tax_id, select_clause, limit, offset)", 'app/views/sparql_templates/environments/has_tax_condition.rq.erb')
       def_erb_method("environments_has_go_condition(prefix, ontology, mpo_id, meo_id, tax_id, bp_id, mf_id, cc_id, select_clause, limit, offset)", 'app/views/sparql_templates/environments/has_go_condition.rq.erb')
 
+      def_erb_method("phenotypes_init_condition(prefix, ontology, select_clause, limit, offset)", 'app/views/sparql_templates/phenotypes/init_condition.rq.erb')
+      def_erb_method("phenotypes_has_mpo_condition(prefix, ontology, mpo_id, select_clause, limit, offset)", 'app/views/sparql_templates/phenotypes/has_mpo_condition.rq.erb')
+      def_erb_method("phenotypes_has_meo_condition(prefix, ontology, mpo_id, meo_id, select_clause, limit, offset)", 'app/views/sparql_templates/phenotypes/has_meo_condition.rq.erb')
+      def_erb_method("phenotypes_has_tax_condition(prefix, ontology, mpo_id, meo_id, tax_id, select_clause, limit, offset)", 'app/views/sparql_templates/phenotypes/has_tax_condition.rq.erb')
+      def_erb_method("phenotypes_has_go_condition(prefix, ontology, mpo_id, meo_id, tax_id, bp_id, mf_id, cc_id, select_clause, limit, offset)", 'app/views/sparql_templates/phenotypes/has_go_condition.rq.erb')
+
       def_erb_method("find_proteins_sparql(prefix, ontology, genes)", 'app/views/sparql_templates/find_proteins.rq.erb')
       def_erb_method("find_gene_ontologies_sparql(prefix, ontology, genes)", 'app/views/sparql_templates/find_gene_ontologies.rq.erb')
       def_erb_method("find_environments_sparql(prefix, ontology, taxonomies)", 'app/views/sparql_templates/find_environments.rq.erb')
@@ -76,6 +96,9 @@ module ReportType
       def_erb_method("find_genome_stats_sparql(prefix, ontology, taxonomies)", 'app/views/sparql_templates/find_genome_stats.rq.erb')
       def_erb_method("find_environment_root_sparql(prefix, ontology, meos)", 'app/views/sparql_templates/find_environment_root.rq.erb')
       def_erb_method("find_environment_inhabitants_stats_sparql(prefix, ontology, meos)", 'app/views/sparql_templates/find_environment_inhabitants_stats.rq.erb')
+
+      def_erb_method("find_phenotype_root_sparql(prefix, ontology, mpos)", 'app/views/sparql_templates/find_phenotype_root.rq.erb')
+      #def_erb_method("find_phenotype_inhabitants_stats_sparql(prefix, ontology, mpos)", 'app/views/sparql_templates/find_phenotype_inhabitants_stats.rq.erb')
     end
   end
 end
