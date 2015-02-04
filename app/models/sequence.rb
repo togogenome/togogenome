fa0474238404aab4679a05c30bb17a6c8a07beac
Naoki Nishiguchi <nnishiguchister@gmail.com>
2015-02-04 17:01:53 +0900

    Sequence name 毎に1行にし、locus_tag, product, Sequence Ontology はリストで出すようにした

--- app/models/sequence.rb
+++ app/models/sequence.rb
@@ -17,14 +17,17 @@ class Sequence
       sparql_results = sparqls.flat_map {|sparql|
         result = query(sparql)
       }
-      # GGGenome のレスポンスの中で、SPARQL で結果が返ってこなかったもの(= name が無いもの)を取得している
-      sparql_hits_refseqs =  sparql_results.map {|g| g[:name] }.uniq
 
-      sparql_not_hits_gggenome_results = gggenome_response['results'].reject {|genome|
-        sparql_hits_refseqs.include?(genome['name'])
-      }.map(&:symbolize_keys)
-
-      sparql_results + sparql_not_hits_gggenome_results
+      gggenome_response['results'].map do |genome|
+        so = sparql_results.select {|r| r[:name] == genome['name'] }
+        genome.merge(
+          {
+            'sequence_ontologies' => so.map {|r| {'uri' => r[:sequence_ontology], 'name' => r[:sequence_ontology_name]}},
+            'locus_tags'          => so.map {|r| r[:locus_tag]}.compact.uniq,
+            'products'            => so.map {|r| r[:product]}.compact.uniq
+          }
+        )
+      end
     end
 
     private
@@ -40,20 +43,20 @@ class Sequence
       # 1. gggenome での検索結果が多い時、それらをまとめて1つのSPARQL にすると、
       #    SPARQLのサーバで 'error code 414: uri too large' にひっかかってしまうため Query を分割している
       # 2. Endpoint 側(hhttp://ep.dbcls.jp/sparql7os) SPARQLに対してタイムアウトが発生し、エラーが帰って来た
-      gggenome_response['results'].each_slice(100).map do |sub_results|
+      gggenome_response['results'].each_slice(300).map do |sub_results|
         <<-SPARQL.strip_heredoc
           DEFINE sql:select-option "order"
           #{SPARQLUtil::PREFIX[:insdc]}
           #{SPARQLUtil::PREFIX[:faldo]}
           #{SPARQLUtil::PREFIX[:obo]}
-          SELECT DISTINCT ?locus_tag ?product ?sequence_ontology ?sequence_ontology_name ?taxonomy ?position ?name ?position_end ?snippet ?snippet_pos ?snippet_end
+          SELECT DISTINCT ?name ?sequence_ontology ?sequence_ontology_name ?locus_tag ?product
           FROM #{SPARQLUtil::ONTOLOGY[:so]}
           FROM #{SPARQLUtil::ONTOLOGY[:refseq]}
           WHERE {
             {
-              SELECT DISTINCT ?sequence_ontology ?sequence_ontology_name ?taxonomy ?position ?name ?position_end ?snippet ?snippet_pos ?snippet_end ?feature
+              SELECT DISTINCT ?name ?sequence_ontology ?sequence_ontology_name ?feature
               WHERE {
-                VALUES (?bioproject ?name ?position ?position_end ?refseq ?snippet ?snippet_end ?snippet_pos ?strand ?taxonomy) {
+                VALUES (?name ?position ?position_end ?refseq ?strand ) {
                   #{bind_values(sub_results)}
                 }
                 ?refseq_uri insdc:sequence_version ?refseq .
@@ -80,7 +83,7 @@ class Sequence
 
     def bind_values(genomes)
       genomes.map {|genome|
-        "( #{genome.map {|key, val| %w(position position_end).include?(key) ? "#{val}" : "\"#{val}\"" }.join(' ')} )"
+        "( #{genome.select {|k, _v| %w(name position position_end refseq strand).include?(k) }.map {|key, val| %w(position position_end).include?(key) ? "#{val}" : "\"#{val}\"" }.join(' ')} )"
       }.join("\n")
     end
   end
