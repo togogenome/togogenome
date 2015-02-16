383948e65e0791c4637f0baba76974b005123c7e
Naoki Nishiguchi <nnishiguchister@gmail.com>
2015-02-16 16:25:31 +0900

    Facet検索 Environment タブも Categroy でソートするようにした

--- app/models/report_type/environment.rb
+++ app/models/report_type/environment.rb
@@ -11,8 +11,8 @@ module ReportType
       end
 
       def search(meo_id: '', tax_id: '', bp_id: '', mf_id: '', cc_id: '', mpo_id: '', limit: 25, offset: 0)
-        select_clause = "SELECT DISTINCT ?meo_id ?meo_name"
-        sparql = build_environment_sparql(meo_id, tax_id, bp_id, mf_id, cc_id, mpo_id, select_clause, limit, offset)
+        select_clause, order_clause = "SELECT DISTINCT ?meo_id ?meo_name ?category ?category_name", "ORDER BY ?category_name"
+        sparql = build_environment_sparql(meo_id, tax_id, bp_id, mf_id, cc_id, mpo_id, select_clause, order_clause, limit, offset)
 
         results = query(sparql)
 
@@ -20,36 +20,29 @@ module ReportType
 
         meos = results.map {|r| "<#{r[:meo_id]}>" }.uniq.join(' ')
 
-        sparqls = [
-          find_environment_root_sparql(PREFIX, ONTOLOGY, meos),
-          find_environment_inhabitants_stats_sparql(PREFIX, ONTOLOGY, meos)
-        ]
+        sparql = find_environment_inhabitants_stats_sparql(PREFIX, ONTOLOGY, meos)
 
-        meo_roots, meo_inhabitants_stats = Parallel.map(sparqls, in_threads: 4) {|sparql|
-          query(sparql)
-        }
+        meo_inhabitants_stats =  query(sparql)
 
         results.map do |result|
-          select_meo_roots = meo_roots.select {|r| r[:meo_id] == result[:meo_id] }
           select_meo_inhabitants_stats = meo_inhabitants_stats.select {|r| r[:meo_id] == result[:meo_id] }
-          new(result, select_meo_roots, select_meo_inhabitants_stats)
+          new(result, select_meo_inhabitants_stats)
         end
       end
     end
 
-    def initialize(meo, meo_roots, meo_inhabitants_stats)
-      @environment, @environment_roots, @meo_inhabitants_stats = meo, meo_roots, meo_inhabitants_stats
+    def initialize(meo, meo_inhabitants_stats)
+      @environment, @meo_inhabitants_stats = meo, meo_inhabitants_stats
     end
 
     def environment
-      root_name = @environment_roots.empty? ? nil : @environment_roots.first[:name]
       count     = @meo_inhabitants_stats.empty? ? nil : @meo_inhabitants_stats.first[:count]
 
-      Struct.new(:uri, :name, :root, :inhabitants){
+      Struct.new(:uri, :name, :category, :inhabitants){
         def id
           uri.split('/').last
         end
-      }.new(@environment[:meo_id], @environment[:meo_name], root_name, count)
+      }.new(@environment[:meo_id], @environment[:meo_name], @environment[:category_name], count)
     end
   end
 end
--- app/views/report_type/environments/index.json.jbuilder
+++ app/views/report_type/environments/index.json.jbuilder
@@ -2,8 +2,8 @@ json.iTotalRecords @total_count
 json.iTotalDisplayRecords @hits_count
 json.aaData do |json|
   json.array!(@results) do |r|
+    json.category         r.environment.category
     json.environment_link link_to(r.environment.name, environment_path(r.environment.id), target: '_blank')
-    json.category         r.environment.root
     json.inhabitants      r.environment.inhabitants
   end
 end
