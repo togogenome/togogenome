97a11c33f8e622c40acda28ac81c43ed43a0a7b6
Naoki Nishiguchi <nnishiguchister@gmail.com>
2015-08-19 18:31:27 +0900

    Convert togogenome-solr clause

--- app/models/stanza_search.rb
+++ app/models/stanza_search.rb
@@ -1,6 +1,65 @@
 require 'open-uri'
 
 class StanzaSearch
+  class SearchQuery
+    def initialize(query)
+      @raw = query
+    end
+
+    attr_reader :raw
+
+    def tokens
+      @tokens ||= begin
+        scanner = StringScanner.new(@raw)
+        scanner.skip /\s+/
+
+        parse(scanner)
+      end
+    end
+
+    def to_clause
+      tokens.each_with_object('') {|token, acc|
+        case token
+        when :lparen
+          acc << '('
+        when :rparen
+          acc << ')'
+        when :or
+          acc << ' OR '
+        when :and
+          acc << ' AND '
+        else
+          acc << "(text:#{token.inspect} OR id_text:#{token.inspect})"
+        end
+      }
+    end
+
+    private
+
+    def parse(scanner, tokens = [])
+      scanner.skip /\s+\z/
+
+      return tokens if scanner.eos?
+
+      tokens <<
+        if scanner.scan(/"((?:[^\\"]|\\.)*)"/)
+          scanner[1].gsub(/\\(.)/, '\1')
+        elsif scanner.scan(/\(\s*/)
+          :lparen
+        elsif scanner.scan(/\s*\)/)
+          :rparen
+        elsif scanner.scan(/\s*OR\s*/)
+          :or
+        elsif scanner.scan(/\s*AND\s*|\s+/)
+          :and
+        else
+          scanner.scan(/\S+(?<!\))/)
+        end
+
+      parse(scanner, tokens)
+    end
+  end
+
   PAGINATE = {per_page: 10}
 
   class << self
@@ -32,8 +91,10 @@ class StanzaSearch
 
     def get_with_cache(stanza_id, q, page)
       begin
+        clause = SearchQuery.new(q).to_clause
+
         solr = RSolr.connect(url: "#{Endpoint.fulltextsearch}/#{stanza_id}")
-        solr.paginate(page, PAGINATE[:per_page], 'select', params: {q: q})
+        solr.paginate(page, PAGINATE[:per_page], 'select', params: {q: clause})
       rescue RSolr::Error::Http => e
         case e.response[:status]
         when 404
