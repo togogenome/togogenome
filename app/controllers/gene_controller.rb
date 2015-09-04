651c1fad322c16c4234e26f072f464f4867df028
Naoki Nishiguchi <nnishiguchister@gmail.com>
2015-09-04 15:36:56 +0900

    Change variable name

--- app/controllers/gene_controller.rb
+++ app/controllers/gene_controller.rb
@@ -1,7 +1,7 @@
 class GeneController < ApplicationController
   def show(id)
     # "1148:slr1311" で1つのid
-    @refseq_id, @gene_id = id.split(':')
+    @tax_id, @gene_id = id.split(':')
     @gene = Gene.find(id)
   end
 end
