a0ac2bf0f34f646a98488b45e5f90b29af1fdd7e
Yu-nomu <nomura@level-five.jp>
2017-07-28 12:24:43 +0900

    change SPARQL

--- app/models/sequence/genome.rb
+++ app/models/sequence/genome.rb
@@ -17,7 +17,7 @@ module Sequence
             sequence_ontologies: so.map {|r| {uri: r[:sequence_ontology], name: r[:sequence_ontology_name]} },
             locus_tags: so.map {|r| r[:locus_tag] }.compact.uniq,
             products: so.map {|r| r[:product] }.compact.uniq
-          ).merge(fetch_genes(genome.taxonomy, genome.position, genome.position_end))
+          ).merge(fetch_genes(genome.refseq, genome.position, genome.position_end))
         end
       end
 
@@ -66,49 +66,87 @@ module Sequence
       private
 
       def bind_values(genomes)
-        genomes.map {|genome|
+        genomes.map { |genome|
           "( \"#{genome.name}\" #{genome.position} #{genome.position_end} \"#{genome.refseq}\" \"#{genome.strand}\" )"
         }.join("\n")
       end
 
-      def fetch_genes(tax_id, result_begin, result_end)
+      def fetch_genes(refseq_id, result_begin, result_end)
         r_begin, r_end = [result_begin.to_i, result_end.to_i].sort
 
-        genes = query(build_gene_sparql(tax_id, r_begin, r_end))
-        prev, other = genes.partition { |g| g[:faldo_begin].to_i < r_begin && g[:faldo_end].to_i < r_begin }
-        nxt, overlap = other.partition { |g| r_end < g[:faldo_begin].to_i && r_end < g[:faldo_end].to_i }
+        ov_genes = query(build_gene_sparql(refseq_id, r_begin, r_end, :overlap))
+        prev_genes = query(build_gene_sparql(refseq_id, r_begin, r_end, :prev))
+        nxt_genes = query(build_gene_sparql(refseq_id, r_begin, r_end, :next))
 
-        { previous: prev, overlap: overlap, next: nxt }
+        {previous: prev_genes, overlap: ov_genes, next: nxt_genes}
       end
 
-      def build_gene_sparql(tax_id, result_begin, result_end)
+      def build_gene_sparql(refseq_id, result_begin, result_end, position)
         <<-SPARQL.strip_heredoc
           #{SPARQLUtil::PREFIX[:insdc]}
           #{SPARQLUtil::PREFIX[:faldo]}
           #{SPARQLUtil::PREFIX[:obo]}
+
           SELECT ?togogenome ?gene_name ?faldo_begin ?faldo_end
           FROM <http://togogenome.org/graph/tgup>
           FROM <http://togogenome.org/graph/refseq>
           WHERE {
-            VALUES (?taxonomy_id ?begin ?end) {
-              ( \"#{tax_id.to_s}\" #{result_begin.to_s} #{result_end.to_s} )
+            VALUES (?refseq_id ?begin ?end) {
+              ( \"#{refseq_id.to_s}\" #{result_begin.to_s} #{result_end.to_s} )
             }
-            ?togogenome rdfs:seeAlso ?taxonomy ;
-                        skos:exactMatch ?feature .
-            ?taxonomy a insdc:Taxonomy ;
-                      rdfs:label ?taxonomy_id .
-            ?feature a insdc:Gene ;
-                     rdfs:label ?gene_name ;
-                     faldo:location ?loc .
-            ?loc faldo:begin/faldo:position ?faldo_begin ;
-                 faldo:end/faldo:position ?faldo_end .
-            FILTER(
-              ?faldo_begin != 1 &&
-              ! ( ( ?faldo_begin < (?begin - 1000) && ?faldo_end < (?begin - 1000) ) || 
-                  ( (?end + 1000) < ?faldo_begin && (?end + 1000) < ?faldo_end ) )
-            )
+            ?refseq_uri rdfs:label ?refseq_id;
+                        insdc:sequence ?seq.
+            ?feature obo:so_part_of ?seq;
+                     rdfs:label ?gene_name;
+                     faldo:location ?loc.
+            ?loc faldo:begin/faldo:position ?faldo_begin;
+                 faldo:end/faldo:position ?faldo_end.
+            ?togogenome skos:exactMatch ?feature .
+            #{make_filter(position)}
           }
+          #{make_order(position)}
+          #{make_limit(position)}
         SPARQL
+      end
+
+      def make_filter(position)
+        case position
+          when :prev
+            <<-EOS
+              FILTER(
+                ?faldo_begin != 1 && (?faldo_begin < ?begin && ?faldo_end < ?begin)
+              )
+            EOS
+          when :next
+            <<-EOS
+              FILTER(
+                ?faldo_begin != 1 && (?begin < ?faldo_begin && ?end < ?faldo_end)
+              )
+            EOS
+          else # :overlap
+            <<-EOS
+              FILTER(
+                ?faldo_begin != 1 &&
+                ! ( (?faldo_begin < ?begin && ?faldo_end < ?begin) ||
+                    (?end < ?faldo_begin && ?end < ?faldo_end) ) ||
+                (?faldo_begin < ?begin && ?end < ?faldo_end )
+              )
+            EOS
+        end
+      end
+
+      def make_order(position)
+        case position
+          when :prev
+            'ORDER BY DESC (?faldo_end)'
+          when :next
+            'ORDER BY ASC (?faldo_begin)'
+        end
+      end
+
+      def make_limit(position)
+        return if position == :overlap
+        'limit 1'
       end
     end
   end
