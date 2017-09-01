56f2389e4d8eaa08c93ea6530d587de6c57fcbfb
Yu-nomu <nomura@level-five.jp>
2017-09-01 12:08:59 +0900

    add to comparative_genome

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,7 @@ module ApplicationHelper
   def page_tabs
     [
       {id: :facet,     name: 'Facet',        path: root_path},
+      {id: :compare,   name: 'Comparative genome', path: compare_path},
       {id: :sequence,  name: 'Sequence',     path: sequence_index_path},
       {id: :converter, name: 'ID converter', path: converter_path},
       {id: :resolver,  name: 'ID resolver',  path: resolver_path},
