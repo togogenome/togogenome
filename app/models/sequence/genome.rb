19c47218b0c119ee4bac84896310844b932c8eeb
Yu-nomu <nomura@level-five.jp>
2017-09-13 04:01:01 +0900

    add search next_gene query & add PREFIX refseq

--- app/models/sequence/genome.rb
+++ app/models/sequence/genome.rb
@@ -8,7 +8,8 @@ module Sequence
       def search(sequence)
         genomes = Sequence::GggenomeSearch.search(sequence)
 
-        sparqls = build_sparqls(genomes)
+        # sparqls = build_sparqls(genomes)
+        sparqls = next_gene_build_sparqls(genomes)
         results = sparqls.flat_map {|sparql| query(sparql) }
 
         genomes.map do |genome|
@@ -18,25 +19,69 @@ module Sequence
             sequence_ontologies: so.map {|r| {uri: r[:sequence_ontology], name: r[:sequence_ontology_name]} },
             locus_tags: so.map {|r| r[:locus_tag] }.compact.uniq,
             products: so.map {|r| r[:product] }.compact.uniq
-          ).merge(fetch_genes(genome.refseq, genome.position, genome.position_end))
-          end
+          )
+        end
       end
 
-      def build_sparqls(gggenome)
+      # def build_sparqls(gggenome)
+      #   gggenome.each_slice(300).map do |sub_results|
+      #     <<-SPARQL.strip_heredoc
+      #       DEFINE sql:select-option "order"
+      #       #{SPARQLUtil::PREFIX[:insdc]}
+      #       #{SPARQLUtil::PREFIX[:faldo]}
+      #       #{SPARQLUtil::PREFIX[:obo]}
+      #       #{SPARQLUtil::PREFIX[:refseq]}
+      #
+      #       SELECT ?refseq_uri ?seq_label ?togogenome ?gene_name ?faldo_begin ?faldo_end
+      #       FROM #{SPARQLUtil::ONTOLOGY[:tgup]}
+      #       FROM #{SPARQLUtil::ONTOLOGY[:refseq]}
+      #       {
+      #         {
+      #           SELECT ?refseq_uri (MAX(?faldo_end) AS ?max_pos)
+      #           FROM #{SPARQLUtil::ONTOLOGY[:tgup]}
+      #           FROM #{SPARQLUtil::ONTOLOGY[:refseq]}
+      #           WHERE {
+      #             VALUES (?refseq_uri ?begin ?end ?dummy1 ?dummy2) {
+      #              #{bind_values(sub_results)}
+      #             }
+      #             ?refseq_uri insdc:sequence ?seq .
+      #             ?feature obo:so_part_of ?seq;
+      #               rdfs:label ?gene_name;
+      #               faldo:location ?loc.
+      #             ?loc faldo:begin/faldo:position ?faldo_begin;
+      #               faldo:end/faldo:position ?faldo_end.
+      #             FILTER(
+      #               ?faldo_begin != 1 && (?faldo_begin < ?begin && ?faldo_end < ?begin)
+      #             )
+      #           } GROUP BY ?refseq_uri
+      #         }
+      #         ?refseq_uri insdc:sequence ?seq ;
+      #           rdfs:label ?seq_label .
+      #         ?feature obo:so_part_of ?seq;
+      #           rdfs:label ?gene_name;
+      #           faldo:location ?loc.
+      #         ?loc faldo:begin/faldo:position ?faldo_begin;
+      #           faldo:end/faldo:position ?faldo_end.
+      #         ?togogenome skos:exactMatch ?feature .
+      #         FILTER( ?faldo_begin = ?max_pos || ?faldo_end = ?max_pos)
+      #       }
+      #     SPARQL
+      # end
+      def next_gene_build_sparqls(gggenome)
         gggenome.each_slice(300).map do |sub_results|
           <<-SPARQL.strip_heredoc
             DEFINE sql:select-option "order"
             #{SPARQLUtil::PREFIX[:insdc]}
             #{SPARQLUtil::PREFIX[:faldo]}
             #{SPARQLUtil::PREFIX[:obo]}
-            PREFIX refseq: <http://identifiers.org/refseq/>
+            #{SPARQLUtil::PREFIX[:refseq]}
 
             SELECT ?refseq_uri ?seq_label ?togogenome ?gene_name ?faldo_begin ?faldo_end
             FROM #{SPARQLUtil::ONTOLOGY[:tgup]}
             FROM #{SPARQLUtil::ONTOLOGY[:refseq]}
             {
               {
-                SELECT ?refseq_uri (MAX(?faldo_end) AS ?max_pos)
+                SELECT ?refseq_uri (MIN(?faldo_begin) AS ?min_pos)
                 FROM #{SPARQLUtil::ONTOLOGY[:tgup]}
                 FROM #{SPARQLUtil::ONTOLOGY[:refseq]}
                 WHERE {
@@ -50,7 +95,7 @@ module Sequence
                   ?loc faldo:begin/faldo:position ?faldo_begin;
                     faldo:end/faldo:position ?faldo_end.
                   FILTER(
-                    ?faldo_begin != 1 && (?faldo_begin < ?begin && ?faldo_end < ?begin)
+                    ?faldo_begin != 1 && (?end < ?faldo_begin && ?end < ?faldo_end)
                   )
                 } GROUP BY ?refseq_uri
               }
@@ -62,13 +107,11 @@ module Sequence
               ?loc faldo:begin/faldo:position ?faldo_begin;
                 faldo:end/faldo:position ?faldo_end.
               ?togogenome skos:exactMatch ?feature .
-              FILTER( ?faldo_begin = ?max_pos || ?faldo_end = ?max_pos)
+              FILTER( ?faldo_begin = ?min_pos || ?faldo_end = ?min_pos)
             }
-            SPARQL
+          SPARQL
         end
       end
-
-
 
       private
 
@@ -76,84 +119,6 @@ module Sequence
         genomes.map { |genome|
           "( refseq:#{genome.refseq} #{genome.position} #{genome.position_end} \"#{genome.name}\" \"#{genome.strand}\" )"
         }.join("\n")
-      end
-
-      def fetch_genes(refseq_id, result_begin, result_end)
-        r_begin, r_end = [result_begin.to_i, result_end.to_i].sort
-
-        ov_genes = query(build_gene_sparql(refseq_id, r_begin, r_end, :overlap))
-        prev_genes = query(build_gene_sparql(refseq_id, r_begin, r_end, :prev))
-        nxt_genes = query(build_gene_sparql(refseq_id, r_begin, r_end, :next))
-
-        {previous: prev_genes, overlap: ov_genes, next: nxt_genes}
-      end
-
-      def build_gene_sparql(refseq_id, result_begin, result_end, position)
-        <<-SPARQL.strip_heredoc
-          #{SPARQLUtil::PREFIX[:insdc]}
-          #{SPARQLUtil::PREFIX[:faldo]}
-          #{SPARQLUtil::PREFIX[:obo]}
-
-          SELECT ?togogenome ?gene_name ?faldo_begin ?faldo_end
-          FROM <http://togogenome.org/graph/tgup>
-          FROM <http://togogenome.org/graph/refseq>
-          WHERE {
-            VALUES (?refseq_id ?begin ?end) {
-              ( \"#{refseq_id.to_s}\" #{result_begin.to_s} #{result_end.to_s} )
-            }
-            ?refseq_uri rdfs:label ?refseq_id;
-                        insdc:sequence ?seq.
-            ?feature obo:so_part_of ?seq;
-                     rdfs:label ?gene_name;
-                     faldo:location ?loc.
-            ?loc faldo:begin/faldo:position ?faldo_begin;
-                 faldo:end/faldo:position ?faldo_end.
-            ?togogenome skos:exactMatch ?feature .
-            #{make_filter(position)}
-          }
-          #{make_order(position)}
-          #{make_limit(position)}
-        SPARQL
-      end
-
-      def make_filter(position)
-        case position
-          when :prev
-            <<-EOS
-              FILTER(
-                ?faldo_begin != 1 && (?faldo_begin < ?begin && ?faldo_end < ?begin)
-              )
-            EOS
-          when :next
-            <<-EOS
-              FILTER(
-                ?faldo_begin != 1 && (?begin < ?faldo_begin && ?end < ?faldo_end)
-              )
-            EOS
-          else # :overlap
-            <<-EOS
-              FILTER(
-                ?faldo_begin != 1 &&
-                ! ( (?faldo_begin < ?begin && ?faldo_end < ?begin) ||
-                    (?end < ?faldo_begin && ?end < ?faldo_end) ) ||
-                (?faldo_begin < ?begin && ?end < ?faldo_end )
-              )
-            EOS
-        end
-      end
-
-      def make_order(position)
-        case position
-          when :prev
-            'ORDER BY DESC (?faldo_end)'
-          when :next
-            'ORDER BY ASC (?faldo_begin)'
-        end
-      end
-
-      def make_limit(position)
-        return if position == :overlap
-        'limit 1'
       end
     end
   end
