bc1f42c7f407a5c47f8667a727695ae0f2ad66ee
Daisuke Satoh <daisuke.satoh@level-five.jp>
2017-09-12 22:48:27 +0900

    just reformat

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
   def page_tabs
     [
-      {id: :facet,     name: 'Facet',        path: root_path},
+      {id: :facet,     name: 'Facet',              path: root_path},
       {id: :compare,   name: 'Comparative genome', path: compare_path},
-      {id: :sequence,  name: 'Sequence',     path: sequence_index_path},
-      {id: :converter, name: 'ID converter', path: converter_path},
-      {id: :resolver,  name: 'ID resolver',  path: resolver_path},
-      {id: :text,      name: 'Text',         path: text_index_path}
+      {id: :sequence,  name: 'Sequence',           path: sequence_index_path},
+      {id: :converter, name: 'ID converter',       path: converter_path},
+      {id: :resolver,  name: 'ID resolver',        path: resolver_path},
+      {id: :text,      name: 'Text',               path: text_index_path}
     ]
   end
 end
