2d7be12dbb632e1df0eebc0fcdd9bb6d016931ed
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2018-07-04 16:10:46 +0900

    update label

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,19 @@
 module ApplicationHelper
   def page_tabs
     [
-      {id: :facet,     name: 'Facet',              path: root_path},
-      {id: :compare,   name: 'Comparative genome', path: compare_path},
-      {id: :sequence,  name: 'Sequence',           path: sequence_index_path},
-      {id: :text,      name: 'Text',               path: text_index_path}
+      { id: :facet,    name: 'Faceted search',     path: root_path },
+      { id: :compare,  name: 'Comparative genome', path: compare_path },
+      { id: :sequence, name: 'Sequence search',    path: sequence_index_path },
+      { id: :text,     name: 'Text search',        path: text_index_path }
     ]
   end
 
   def report_tabs
     [
-      {type: 'organism'},
-      {type: 'gene'},
-      {type: 'phenotype'},
-      {type: 'environment'}
+      { type: 'organism' },
+      { type: 'gene' },
+      { type: 'phenotype' },
+      { type: 'environment' }
     ]
   end
 end
